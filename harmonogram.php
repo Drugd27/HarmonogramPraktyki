04a533dae593279a3989ca667605944150e2da94
Drugd27 <maciejsalek2007@gmail.com>
2025-03-20 14:12:49 +0100

    Add files via upload

--- harmonogram.php
+++ harmonogram.php
@@ -3,38 +3,19 @@
 <head>
     <meta charset="utf-8">
     <title>Harmonogram pracy</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="script" href="skrypt.js">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="script" href="./skrypt.js">
+</head>
 <body>
 <section>
-    od: <input type="date">
-    do: <input type="date">
-
-    <input type="checkbox"> Wyświetl opinie
+    <form action="harmonogram.php" method="POST" id="filtry">
+        od: <input type="date" name="data_od">
+        do: <input type="date" name="data_do">
+        <input type="checkbox" id="opis"> Wyświetl opisy prac
+        <input type="submit" value="Filtruj" onclick="filtruj()">
+    </form>
+    
     <br><br>
-</section>
-<section>
-    <table id="tabela">
-        <tr>
-            <th>Data</th>
-        </tr>
-        <tr>
-            
-        </tr>
-        <tr>
-
-        </tr>
-        <tr>
-
-        </tr>
-        <tr>
-
-        </tr>
-        <tr>
-
-        </tr>
-
-    </table>
 </section>
 
 <?php
@@ -49,23 +30,44 @@
         die("Nie połączono z bazą danych: " . $conn -> connect_error);
     }
 
-    $sql = "SELECT Pracownik, Data,  Godzina, GROUP_CONCAT(DISTINCT Firma, ' ', ID_Uslugi SEPARATOR ', <br>') AS Firma FROM `harmonogram` 
-            GROUP BY Pracownik";
+    $sql = "SELECT Data, 
+                   GROUP_CONCAT(Pracownik SEPARATOR ', ') AS Pracownicy, 
+                   GROUP_CONCAT(Firma SEPARATOR ', ') AS Firmy, 
+                   Godzina 
+            FROM `harmonogram` 
+            GROUP BY Data";
 
-    $result = $conn -> query($sql);
+    $result = $conn->query($sql);
 
     echo "<table id='tabela'>";
-    if($result -> num_rows > 0){
-        while($row = $result -> fetch_assoc()){
-        
-            echo "<th>" . $row["Data"] . "</th>";
-            
-            echo "<tr>";
-                echo"<td>" . $row["Pracownik"] . "</td>";
-                echo"<td>" . $row["Firma"]. "</td>";
-                echo"<td>" .  $row["Godzina"]. "</td>";
+    if ($result->num_rows > 0) {
+        echo "<tr>";
+        echo "<th>Data</th>";
+        echo "<th>Pracownicy</th>";
+        echo "<th>Firmy</th>";
+        echo "<th>Godzina</th>";
+        echo "</tr>";
 
-            echo "</tr>";
+        while ($row = $result->fetch_assoc()) {
+            // Split Pracownicy and Firmy into arrays
+            $pracownicy = explode(', ', $row["Pracownicy"]);
+            $firmy = explode(', ', $row["Firmy"]);
+            $godziny = explode
+
+            // Ensure both arrays have the same length
+            $maxRows = max(count($pracownicy), count($firmy));
+
+            for ($i = 0; $i < $maxRows; $i++) {
+                echo "<tr>";
+                // Display Data and Godzina only for the first row of each group
+                if ($i == 0) {
+                    echo "<td rowspan='$maxRows'>" . $row["Data"] . "</td>";
+                    echo "<td rowspan='$maxRows'>" . $row["Godzina"] . "</td>";
+                }
+                echo "<td>" . ($pracownicy[$i] ?? '') . "</td>";
+                echo "<td>" . ($firmy[$i] ?? '') . "</td>";
+                echo "</tr>";
+            }
         }
     } else {
         echo "<tr><td colspan='4'>Brak danych w bazie danych</td></tr>";
---
+++ skrypt.js
@@ -0,0 +1,5 @@
+function filtruj() {
+
+    let opis = document.getElementById("opis");
+
+}
